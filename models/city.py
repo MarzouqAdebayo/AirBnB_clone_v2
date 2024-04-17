7f2b15b2836906448fcbd0e1b61b76e259ce86f6
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-04-17 17:24:43 +0000

    Completed task 8 with files models/places.py, models/user.py, models/city.py

--- models/city.py
+++ models/city.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 from os import getenv
 
 
@@ -18,6 +18,8 @@ class City(BaseModel, Base):
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', backref='cities',
+                              cascade='all, delete-orphan')
     else:
         name = ""
         state_id = ""
