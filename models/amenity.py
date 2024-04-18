928f46d7d5363cd8aabe77f4b67c901c7b65b05b
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-04-18 02:56:33 +0000

    Fixed bug

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-import models
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
@@ -21,6 +20,6 @@ class Amenity(BaseModel, Base):
     if (storage_type == 'db'):
         name = Column(String(128), nullable=False)
         place_amenities = relationship('Place', back_populates="amenities",
-                                       secondary=models.place.place_amenity)
+                                       secondary='place_amenity')
     else:
         name = ""
