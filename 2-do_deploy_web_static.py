5c403ae64403729b08a890c5a3ffd52d8c6a3da8
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-05-06 23:40:48 +0000

    task 2 bug fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -30,24 +30,27 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """
-    Deploys archive to all web servers
-    """
-    if os.path.exists(archive_path) is False:
+    """Distributes an archive to your web servers."""
+    if not os.path.exists(archive_path):
         return False
 
-    try:
-        archive_filename = archive_path.split('/')[-1]
-        put(archive_path, '/tmp/')
-        release_dir = '/data/web_static/releases/{}'.format(
-            archive_filename.split('.')[0])
-        run('mkdir -p {}'.format(release_dir))
-        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_dir))
-        run('rm /tmp/{}'.format(archive_filename))
-        run('mv {}/web_static/* {}'.format(release_dir, release_dir))
-        run('rm -rf {}/web_static'.format(release_dir))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(release_dir))
-        return True
-    except Exception as e:
-        return False
+    archive_filename = os.path.basename(archive_path)
+    archive_name_without_ext = os.path.splitext(archive_filename)[0]
+
+    put(archive_path, f'/tmp/{archive_filename}', use_sudo=True)
+
+    release_dir = f"/data/web_static/releases/{archive_name_without_ext}"
+    sudo(f'mkdir -p {release_dir}')
+    sudo(f'tar -xzf /tmp/{archive_filename} -C {release_dir}/')
+
+    sudo(f'rm /tmp/{archive_filename}')
+    # Move uncompressed file's content
+    sudo(f'mv {release_dir}/web_static/* {release_dir}/')
+    # Remove uncompressed folder
+    sudo(f'rm -rf {release_dir}/web_static/')
+    # Remove old symlink
+    sudo('rm -rf /data/web_static/current')
+    # Create new symlink
+    sudo(f'ln -s {release_dir}/ "/data/web_static/current"')
+
+    return True
