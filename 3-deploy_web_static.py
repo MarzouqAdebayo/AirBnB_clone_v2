6d34d27316f9187e6ce7fae287a12b81d3bd0227
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-05-06 23:46:48 +0000

    Task 3 done

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive from the contents of the
+web_static folder and deploys it to web server
+"""
+from fabric.api import *
+import os.path
+import tarfile
+from datetime import datetime
+
+
+env.hosts = ['54.242.192.138', '3.90.85.81']
+env.user = 'ubuntu'
+
+
+def do_pack():
+    """
+    Generates a .tgz file
+    """
+    local("mkdir -p versions")
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    archive_name = f'web_static_{timestamp}.tgz'
+    archive_path = os.path.join('versions', archive_name)
+    local(f'tar -czf {archive_path} web_static/')
+
+    if os.path.exists(archive_path):
+        return archive_path
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    """Distributes an archive to your web servers."""
+    if not os.path.exists(archive_path):
+        return False
+
+    archive_filename = os.path.basename(archive_path)
+    archive_name_without_ext = os.path.splitext(archive_filename)[0]
+
+    put(archive_path, f'/tmp/{archive_filename}', use_sudo=True)
+
+    release_dir = f"/data/web_static/releases/{archive_name_without_ext}"
+    sudo(f'mkdir -p {release_dir}')
+    sudo(f'tar -xzf /tmp/{archive_filename} -C {release_dir}/')
+
+    sudo(f'rm /tmp/{archive_filename}')
+    # Move uncompressed file's content
+    sudo(f'mv {release_dir}/web_static/* {release_dir}/')
+    # Remove uncompressed folder
+    sudo(f'rm -rf {release_dir}/web_static/')
+    # Remove old symlink
+    sudo('rm -rf /data/web_static/current')
+    # Create new symlink
+    sudo(f'ln -s {release_dir}/ "/data/web_static/current"')
+
+    return True
+
+
+def deploy():
+    """ Creates and distributes the archive to the web servers"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+
+    return do_deploy(archive_path)
