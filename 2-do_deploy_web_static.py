ac3c1c407100c4cb5e0ddf991a2b721a20138d30
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-05-06 22:54:27 +0000

    Bug fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,27 +1,53 @@
 #!/usr/bin/python3
 """
-Deploys static to the web server
+Fabric script that generates a .tgz archive from the contents of the
+web_static folder
 """
-from fabric.api import env, put, sudo
-import os
+from fabric.api import *
+import os.path
+import tarfile
+from datetime import datetime
 
-env.hosts = ['54.242.192.138', '3.90.85.81']
-env.user = 'ubuntu'
+
+env.hosts = []
+env.name = 'ubuntu'
+
+
+def do_pack():
+    """
+    Generates a .tgz file
+    """
+    local("mkdir -p versions")
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    archive_name = f'web_static_{timestamp}.tgz'
+    archive_path = os.path.join('versions', archive_name)
+    local(f'tar -czf {archive_path} web_static/')
+
+    if os.path.exists(archive_path):
+        return archive_path
+    else:
+        return None
+
 
 def do_deploy(archive_path):
     """
-    Distributes an archive to the web servers
+    Deploys archive to all web servers
     """
-    if not os.path.exists(archive_path):
+    if os.path.exists(archive_path) is False:
         return False
+    try:
+        put(archive_path, '/tmp/', use_sudo=True)
+        archive_filename = archive_path.split('/')[-1]
+        release_dir = '/data/web_static/releases/{}'.format(
+            archive_filename.split('.')[0])
+        sudo('mkdir -p {}').format(release_dir)
+        sudo('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_dir))
 
-    put(archive_path, '/tmp/')
-    archive_filename = os.path.basename(archive_path)
-    archive_folder = os.path.splittext(archive_filename)[0]
+        sudo('rm /tmp/{}'.format(archive_filename))
+        sudo('rm /data/web_static/current')
 
-    sudo(f'tar -xzf /tmp/{archive_filename} -C /data/web_static/releases/')
-    sudo(f'rm /tmp/{archive_filename}')
-    sudo(f'rm -rf /data/web_static/current')
-    sudo(f'ln -s /data/web_static/releases/{archive_folder} /data/web_static/current')
+        sudo('ln -s {} /data/web_static/current'.format(release_dir))
 
-    return True
+        return True
+    except Exception:
+        return False
