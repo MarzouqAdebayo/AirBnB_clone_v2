067e647073522c1de072fecbb2f207dfe955d3f0
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-05-06 18:42:57 +0000

    Task 2 correction

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-from fabric.api import env, put, run
+"""
+Deploys static to the web server
+"""
+from fabric.api import env, put, sudo
 import os
 
 env.hosts = ['54.242.192.138', '3.90.85.81']
@@ -16,9 +19,9 @@ def do_deploy(archive_path):
     archive_filename = os.path.basename(archive_path)
     archive_folder = os.path.splittext(archive_filename)[0]
 
-    run(f'tar -xzf /tmp/{archive_filename} -C /data/web_static/releases/')
-    run(f'rm /tmp/{archive_filename}')
-    run(f'rm -rf /data/web_static/current')
-    run(f'ln -s /data/web_static/releases/{archive_folder} /data/web_static/current')
+    sudo(f'tar -xzf /tmp/{archive_filename} -C /data/web_static/releases/')
+    sudo(f'rm /tmp/{archive_filename}')
+    sudo(f'rm -rf /data/web_static/current')
+    sudo(f'ln -s /data/web_static/releases/{archive_folder} /data/web_static/current')
 
     return True
