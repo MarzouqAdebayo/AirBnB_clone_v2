1d30ea17c69a9f5a0ae2f185c9c1e24169bf146e
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-05-07 04:45:02 +0000

    Task 2 bug fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -29,35 +29,27 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """distributes an archive to your web servers
+    """ function distrubtes an archive to my web servers
     """
-    if os.path.exists(archive_path) is False:
+    path_existence = os.path.exists(archive_path)
+    if path_existence is False:
         return False
-
-    arc = archive_path.split("/")
-    arc_file_without_ext = arc[1].strip('.tgz')
-
-    # upload the archive to /tmp/
-    put(archive_path, '/tmp/{}'.format(arc[1]), use_sudo=True)
-
-    # uncompress the archive
-    sudo('mkdir -p /data/web_static/releases/{}'.format(arc_file_without_ext))
-    main = "/data/web_static/releases/{}".format(arc_file_without_ext)
-    sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
-
-    # delete the archive file
-    sudo('rm /tmp/{}'.format(arc[1]))
-
-    # move uncompressed file's content
-    sudo('mv {}/web_static/* {}/'.format(main, main))
-
-    # remove uncompressed folder
-    sudo('rm -rf {}/web_static/'.format(main))
-
-    # remove old symlink
-    sudo('rm -rf /data/web_static/current')
-
-    # create new symlink
-    sudo('ln -s {}/ "/data/web_static/current"'.format(main))
-
-    return True
+    try:
+        path_split = archive_path.replace('/', ' ').replace('.', ' ').split()
+        just_directory = path_split[0]
+        no_tgz_name = path_split[1]
+        full_filename = path_split[1] + '.' + path_split[2]
+        folder = '/data/web_static/releases/{}/'.format(no_tgz_name)
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}'.format(folder))
+        run('tar -xzf /tmp/{} -C {}'.format(full_filename, folder))
+        run('rm /tmp/{}'.format(full_filename))
+        run('mv {}web_static/* {}'.format(folder, folder))
+        run('rm -rf {}web_static'.format(folder))
+        current = '/data/web_static/current'
+        run('rm -rf {}'.format(current))
+        run('ln -s {} {}'.format(folder, current))
+        print("New version deployed!")
+        return True
+    except Exception:
+        return False
