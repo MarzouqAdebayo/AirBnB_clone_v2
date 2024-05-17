65705247b5383563ca6ff4af427a80884845909b
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-05-17 12:33:46 +0000

    Checkpoint commit

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module "7-states.py"
+"""
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """
+    Function states_list that handles "/states_list" route
+    """
+    states = storage.all('State').values()
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def handle_teardown():
+    """
+    method executed during teardown
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
